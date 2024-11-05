851f8833bfa1488a0958bdf853a7947cf7bb3b94
danitrapero10 <danitrape@gmail.com>
2024-11-05 14:30:43 +0100

    seguir

---
+++ Programación/Ejercicios/Registro.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+struct estudiante {
+	char nombre [50];
+	int edad;
+        float promedio;
+}	
+int main(){
+	printf("Introduce el nombre de un estudiante:");
+	scanf ("%d\n"
+
+}
